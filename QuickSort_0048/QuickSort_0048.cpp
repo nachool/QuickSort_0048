80b0f166f0391927ab4f0dfe139b4377fd839fc5
nachool <nachma.olivia1394@gmail.com>
2024-03-28 19:13:34 +0700

    memanggil input, q_sort, display pada int main

--- QuickSort_0048/QuickSort_0048.cpp
+++ QuickSort_0048/QuickSort_0048.cpp
@@ -104,7 +104,18 @@ int main()
     char ch;
 
     do {
+        input(); 
+        q_sort(0, n - 1);
+        display();
+        cout << "\n\nDo you want to continue? (y/n): ";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
 
+        system("pause");
+        system("cls");
     } while (true);
+
+    return 0;
 }
 

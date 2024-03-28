618450c1e0e915b4fb9c482f655457408254bc77
nachool <nachma.olivia1394@gmail.com>
2024-03-28 19:03:03 +0700

    display sorted array

--- QuickSort_0048/QuickSort_0048.cpp
+++ QuickSort_0048/QuickSort_0048.cpp
@@ -86,6 +86,10 @@ void q_sort(int low, int high)
 }
 
 void display() {
+    cout << "\n---------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "------------------" << endl;
+
 
 }
 
